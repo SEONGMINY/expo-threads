07e3bbc9953d4321d5bac6d5defe1bc32a547124
SEONGMINY <leesm2075@naver.com>
2024-11-29 17:32:07 +0900

    feat: social login

---
+++ app/(public)/index.tsx
@@ -0,0 +1,180 @@
+import { Colors } from "@/constants/Colors";
+import { useOAuth } from "@clerk/clerk-expo";
+import React from "react";
+import {
+  Image,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+import useI18n from "@/i18n/hooks/useI18n";
+
+const Index = () => {
+  const { t } = useI18n();
+
+  const { startOAuthFlow: startFacebookOAuthFlow } = useOAuth({
+    strategy: "oauth_facebook",
+  });
+
+  const { startOAuthFlow: startGoogleOAuthFlow } = useOAuth({
+    strategy: "oauth_google",
+  });
+
+  const onClickInstagramLogin = async () => {
+    try {
+      const { createdSessionId, setActive } = await startFacebookOAuthFlow();
+
+      if (createdSessionId) {
+        setActive!({ session: createdSessionId });
+      }
+    } catch (err) {
+      console.error("OAuth error", err);
+    }
+  };
+
+  const onClickGoogleLogin = async () => {
+    try {
+      const { createdSessionId, setActive } = await startGoogleOAuthFlow();
+
+      if (createdSessionId) {
+        setActive!({ session: createdSessionId });
+      }
+    } catch (err) {
+      console.error("OAuth error", err);
+    }
+  };
+
+  return (
+    <View style={styles.container}>
+      <Image
+        source={require("@/assets/images/login.png")}
+        style={styles.loginImage}
+      />
+      <ScrollView contentContainerStyle={styles.container}>
+        <Text style={styles.title}>{t("loginPageTitle")}</Text>
+
+        <View style={styles.buttonContainer}>
+          <TouchableOpacity
+            style={styles.loginButton}
+            onPress={onClickInstagramLogin}
+          >
+            <View style={styles.loginButtonContent}>
+              <Image
+                source={require("@/assets/images/instagram_icon.webp")}
+                style={styles.loginButtonImage}
+              />
+              <Text style={styles.loginButtonText}>
+                {t("instagramLoginText")}
+              </Text>
+              <Ionicons
+                name="chevron-forward"
+                size={24}
+                color={Colors.border}
+              />
+            </View>
+            <Text style={styles.loginButtonSubtitle}>
+              {t("instagramLoginDescription")}
+            </Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity
+            style={styles.loginButton}
+            onPress={onClickGoogleLogin}
+          >
+            <View style={styles.loginButtonContent}>
+              <Text style={styles.loginButtonText}>{t("googleLoginText")}</Text>
+              <Ionicons
+                name="chevron-forward"
+                size={24}
+                color={Colors.border}
+              />
+            </View>
+          </TouchableOpacity>
+
+          <TouchableOpacity style={styles.loginButton}>
+            <View style={styles.loginButtonContent}>
+              <Text style={styles.loginButtonText}>
+                {t("useWithoutProfileText")}
+              </Text>
+              <Ionicons
+                name="chevron-forward"
+                size={24}
+                color={Colors.border}
+              />
+            </View>
+            <Text style={styles.loginButtonSubtitle}>
+              {t("useWithoutProfileDescription")}
+            </Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity>
+            <Text style={styles.switchAccountButtonText}>
+              {t("switchAccountsText")}
+            </Text>
+          </TouchableOpacity>
+        </View>
+      </ScrollView>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    gap: 20,
+    backgroundColor: Colors.background,
+  },
+  loginImage: {
+    width: "100%",
+    height: 350,
+    resizeMode: "cover",
+  },
+  title: {
+    fontSize: 17,
+    fontFamily: "DMSans_500Medium",
+  },
+  buttonContainer: {
+    gap: 20,
+    marginHorizontal: 20,
+  },
+  loginButton: {
+    backgroundColor: "#fff",
+    paddingVertical: 20,
+    paddingHorizontal: 20,
+    borderRadius: 8,
+    borderWidth: StyleSheet.hairlineWidth,
+    borderColor: Colors.border,
+  },
+  loginButtonText: {
+    color: "#000",
+    fontSize: 15,
+    fontFamily: "DMSans_500Medium",
+    flex: 1,
+  },
+  loginButtonContent: {
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 10,
+  },
+  loginButtonImage: {
+    width: 50,
+    height: 50,
+  },
+  loginButtonSubtitle: {
+    fontSize: 12,
+    fontFamily: "DMSans_400Regular",
+    color: "#acacac",
+    marginTop: 5,
+  },
+  switchAccountButtonText: {
+    fontSize: 14,
+    color: Colors.border,
+    alignSelf: "center",
+  },
+});
+
+export default Index;
