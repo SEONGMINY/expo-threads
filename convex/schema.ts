5e7fd1cb1bd06d60b56821cd9fe5d9cf435c4668
SEONGMINY <leesm2075@naver.com>
2024-11-29 18:19:14 +0900

    feat: message schema

--- convex/schema.ts
+++ convex/schema.ts
@@ -15,6 +15,26 @@ export const User = {
   pushToken: v.optional(v.string()),
 };
 
+/**
+ * Message schema
+ * userId: users 테이블에 대한 외래 키
+ * likeCount: 좋아요 수 @기본값 0
+ * commentCount: 댓글 수 @기본값 0
+ * retweetCount: 리트윗 수 @기본값 0
+ * mediaFiles: 미디어 파일 URL 배열
+ */
+export const Message = {
+  userId: v.id("users"),
+  threadId: v.optional(v.string()),
+  content: v.string(),
+  likeCount: v.number(),
+  commentCount: v.number(),
+  retweetCount: v.number(),
+  mediaFiles: v.optional(v.array(v.string())),
+  websiteUrl: v.optional(v.string()),
+};
+
 export default defineSchema({
   users: defineTable(User),
+  messages: defineTable(Message),
 });
